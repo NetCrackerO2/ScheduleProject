9769876ea088241a4eaab4eb6410fd93b418ba7a
AlexKaryasov <hypotalamus95@gmail.com>
2016-11-09 21:09:42 +0400

    RA was removed

--- src/professorstudent/Account.java
+++ src/professorstudent/Account.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -17,7 +17,6 @@ public class Account {
     private String name;
     private Cathedra cathedra;
     private Group group;
-    private RoleAssigment roleAssigment;
 
     public Account(String name, Group group) {
         this.name = name;
