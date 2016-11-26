9e6cf37c6b291a7de4273d7a14991e450c331cd7
temon137 <ak973@yandex.ru>
2016-11-26 14:40:15 +0400

    Добавлено перечисление Permission - перечень доступных аккаунтам прав. Переделан класс Role - он хранит список принадлежащих ему прав и позволяет работать с ним. Добавлен класс RoleManager - хранит список всех ролей и их связи с аккаунтами и позволяет работать с ними.

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,27 @@
-import faculty.FacultyManager;
-import mvc.Commands.*;
-import mvc.Controller;
 import account.Account;
 import account.AccountManager;
-import cathedra.Cathedra;
-import cathedra.CathedraManager;
-import faculty.Faculty;
-import faculty.FacultyImpl;
-import group.Group;
-import group.GroupsManager;
-import subject.Subject;
-import subject.SubjectManager;
+import account.Permission;
+import account.RoleManager;
 
 
 public class Main {
-    public static void main(String[] args) {
-        CathedraManager cathedraManager = CathedraManager.getInstance();
-        GroupsManager groupsManager = GroupsManager.getInstance();
-        AccountManager accountManager = AccountManager.getInstance();
-        SubjectManager subjectManager = SubjectManager.getInstance();
+	public static void main(String[] args) {
+		AccountManager accountManager = AccountManager.getInstance();
+		RoleManager roleManager = RoleManager.getInstance();
 
-        Faculty faculty1, faculty2;
-        faculty1 = new FacultyImpl(1);
-        faculty2 = new FacultyImpl(2);
+		Account account = accountManager.getNewAccount("This Is ACCOUNT");
 
-        // Распределяем кафедры по факультетам
-        Cathedra cathedra1, cathedra2, cathedra3;
-        cathedra1 = cathedraManager.addNewCathedra(faculty1,"Phisics");
-        cathedra2 = cathedraManager.addNewCathedra(faculty2,"IT");
-        cathedra3 = cathedraManager.addNewCathedra(faculty2,"Garbage");
+		roleManager.createRole("Role#1");
+		roleManager.getRole("Role#1").addPermissions(Permission.Student);
+		roleManager.createRole("Role#2");
+		roleManager.getRole("Role#2").addPermissions(Permission.NoStudent);
+		roleManager.createRole("Role#3");
 
-        // Распределяем группы по факультетам и кафедрам
-        Group[] groups = new Group[10];
-        for (int i = 0; i < 10; i++)
-            groups[i] = groupsManager.getNewGroup(
-                    i % 2 == 0 ? cathedra2 : cathedra1,
-                    i + 1000
-            );
+		roleManager.addRole(account, roleManager.getRole("Role#1"));
 
-        // Распределяем аккаунты по группам
-        Account[] accounts = new Account[10];
-        for (int i = 0; i < 10; i++)
-            accounts[i] = accountManager.getNewAccount("" + i, i < 5 ? groupsManager.getGroup(1003) : groupsManager.getGroup(1004));
-
-        // Распределяем предметы по кафедрам
-        Subject[] subjects = new Subject[10];
-        for (int i = 0; i < 10; i++)
-            subjects[i] = subjectManager.getNewSubject(i < 5 ? cathedra1 : cathedra2, "" + i);
-
-        faculty1=FacultyManager.getInstance().addNewFaculty(1);
-        CathedraManager.getInstance().addNewCathedra(faculty1,"Test");
-
-        Controller controller = new Controller();
-        controller.addCommand(new FullAccountsCommand());
-        controller.addCommand(new ViewListGroupsCommand());
-        controller.addCommand(new ViewListSubjectsCommand());
-        controller.addCommand(new ViewListCathedraCommand());
-        controller.addCommand(new ViewListFacultiesCommand());
-        controller.addCommand(new AddNewCathedraCommand());
-        controller.addCommand(new AddNewGroupCommand());
-        controller.addCommand(new AddNewSubjectCommand());
-        controller.addCommand(new AddNewFacultyCommand());
-        controller.addCommand(new EditGroupCommand());
-        controller.addCommand(new EditCathedraCommand());
-        controller.addCommand(new RemoveGroupCommand());
-        controller.addCommand(new RemoveCathedraCommand());
-
-        controller.start();
-    }
+		System.out.println(roleManager.hasRole(account, "Role#1"));
+		System.out.println(roleManager.hasRole(account, "Role#2"));
+		System.out.println(roleManager.hasPermission(account, Permission.Student));
+		System.out.println(roleManager.hasPermission(account, Permission.NoStudent));
+	}
 }
---
+++ src/account/RoleManager.java
@@ -0,0 +1,160 @@
+package account;
+
+
+import java.util.*;
+
+
+public class RoleManager {
+	private static RoleManager instance;
+	private List<Role> roles;
+	private Map<Account, List<Role>> map;
+
+
+	private RoleManager() {
+		roles = new ArrayList<>();
+		map = new HashMap<>();
+	}
+
+	/**
+	 * Singletone instance
+	 *
+	 * @return new RoleManager if called before, buffered one otherwise
+	 */
+	public static RoleManager getInstance() {
+		if (instance == null)
+			instance = new RoleManager();
+		return instance;
+	}
+
+	/**
+	 * Проверка существования роли в некотором списке ролей
+	 *
+	 * @param name Имя искомой роли
+	 * @param list Список, в котором ищестя роль
+	 * @return true - роль найдена, иначе false
+	 */
+	private boolean isExistInList(String name, List<Role> list) {
+		return list.stream().anyMatch(currentRole -> Objects.equals(currentRole.getName(), name));
+	}
+
+	/**
+	 * Получение списка ролей аккаунта. Если его нет, то создатся новый
+	 *
+	 * @param account Объект аккаунта, для которого ищется список ролей
+	 * @return Список ролей указанного аккаунта
+	 */
+	private List<Role> getOrCreateRoleList(Account account) {
+		List<Role> list = map.get(account);
+
+		if (list == null) {
+			map.put(account, list = new ArrayList<>());
+		}
+
+		return list;
+	}
+
+	/**
+	 * Создание новой роли
+	 *
+	 * @param name Имя новой роли
+	 * @return Объект созданной роли
+	 */
+	public Role createRole(String name) {
+		// TODO: заменить на более подходящий тип исключения
+		if (isExist(name))
+			throw new RuntimeException("Роль с таким именем уже существует.");
+
+		Role newRole = new Role(name);
+		roles.add(newRole);
+
+		return newRole;
+	}
+
+
+	/**
+	 * Связывание аккаунта и роли
+	 *
+	 * @param account Объект аккаунта, к которому будет привязана роль
+	 * @param role    Объект роли, который будет привязан к аккаунту
+	 */
+	public void addRole(Account account, Role role) {
+		// TODO: название метода необходимо изменить на более отражающее его назначение
+		List<Role> list = getOrCreateRoleList(account);
+
+		if (isExistInList(role.getName(), list))
+			// TODO: исключение?..
+			return;
+
+		list.add(role);
+
+		map.put(account, list);
+	}
+
+	/**
+	 * Получение роли по её имени
+	 *
+	 * @param name Имя искомой роли
+	 * @return Объект роли с указанным именем
+	 */
+	public Role getRole(String name) {
+		for (Role current : roles)
+			if (current.getName().equals(name))
+				return current;
+
+		throw new NoSuchElementException();
+	}
+
+	/**
+	 * Получение ролей указанного аккаунта
+	 *
+	 * @param account Объект аккаунта, роли которого необходимо получить
+	 * @return Список ролей указанного аккаунта
+	 */
+	public List<Role> getRoles(Account account) {
+		return new ArrayList<>(getOrCreateRoleList(account));
+	}
+
+	/**
+	 * Проверка на принадлежность аккаунта конкретной роли
+	 *
+	 * @param account Объект проверяемого аккаунта
+	 * @param name    Название роли, принадлежность которой проверяется
+	 * @return true - аккаунт является обладателем указанной роли, иначе false
+	 */
+	public boolean hasRole(Account account, String name) {
+		// TODO: выбрасывать ли исключение при несуществовании указанной роли?
+		// TODO: принимать имя роли или её объект?
+		// TODO: нужно нормальное имя
+		return isExistInList(name, getRoles(account));
+	}
+
+	/**
+	 * Проверка принадлежности права аккаунту
+	 *
+	 * @param account    Объект аккаунта, принадлежность которому проверяется
+	 * @param permission Право, принадлежность которого проверяется
+	 * @return true - право принадлежит аккаунту, иначе false
+	 */
+	public boolean hasPermission(Account account, Permission permission) {
+		return getRoles(account).stream().anyMatch(role -> role.hasPermission(permission));
+	}
+
+	/**
+	 * Полечение списка всех существующих ролей
+	 *
+	 * @return Список всех существующих ролей
+	 */
+	public List<Role> getAllRoles() {
+		return new ArrayList<>(roles);
+	}
+
+	/**
+	 * Проверка существования роли с указанным именем
+	 *
+	 * @param name Имя искомой роли
+	 * @return true - роль существует, иначе false
+	 */
+	public boolean isExist(String name) {
+		return isExistInList(name, roles);
+	}
+}
